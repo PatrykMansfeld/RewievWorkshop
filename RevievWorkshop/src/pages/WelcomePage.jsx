1da8c1ed40a8a61ffa7041d84100b33bb5bb76e3
Patryk_Mansfeld <patryk.mansfeld@gmail.com>
2024-09-19 15:04:24 +0200

    Created WelcomePage

---
+++ RevievWorkshop/src/pages/WelcomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function WelcomePage() {
+  return (
+    <div>
+      <div className='WelcomePageBox'>
+        <h1 className='WelcomePageTitle'>Review Workshop!</h1>
+        <p>
+          Welcome to the center of all revies where you cna find reviews ranging from movies to games and even books!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomePage
